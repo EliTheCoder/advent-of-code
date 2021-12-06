f2b7acce733ecaab9c670e9dac7da1a882909ce8
EliTheCoder <ex.games.org@gmail.com>
2021-12-06 11:51:36 -0600

    day5

--- day5/main.ts
+++ day5/main.ts
@@ -5,8 +5,8 @@ const text = await Deno.readTextFile(
 );
 const lines = text.split("\n");
 
-const ventPoints: Point[] = [];
-const crossPoints: Point[] = [];
+let ventPoints: Point[] = [];
+let crossPoints: Point[] = [];
 
 class Point {
 	constructor(public x: number, public y: number) {}
@@ -22,7 +22,19 @@ enum LineType {
 }
 
 class Line {
-	constructor(public start: Point, public end: Point) {}
+	public start: Point;
+	public end: Point;
+
+	constructor(start: Point, end: Point) {
+		if (start.x > end.x) {
+			[start, end] = [end, start];
+		}
+		if (start.y > end.y) {
+			[start, end] = [end, start];
+		}
+		this.start = start;
+		this.end = end;
+	}
 
 	span(): Point[] {
 		const points = [];
@@ -35,10 +47,16 @@ class Line {
 				points.push(new Point(this.start.x, y));
 			}
 		} else if (this.type() === LineType.DIAGONAL) {
-			for (let x = this.start.x, y = this.start.y; x <= this.end.x; x++) {
+			let x = this.start.x;
+			let y = this.start.y;
+			while (x !== this.end.x) {
 				points.push(new Point(x, y));
-				y++;
+				if (x < this.end.x) x++;
+				if (x > this.end.x) x--;
+				if (y < this.end.y) y++;
+				if (y > this.end.y) y--;
 			}
+			points.push(new Point(x, y));
 		}
 		return points;
 	}
@@ -56,16 +74,13 @@ class Line {
 	}
 }
 
-const vents = lines
-	.map(line => {
-		const lineArr = line.split(" -> ").map(point => {
-			const pointArr = point.split(",").map(Number);
-			return new Point(pointArr[0], pointArr[1]);
-		});
-		return new Line(lineArr[0], lineArr[1]);
-	})
-	.slice(0, -1)
-	.filter(line => line.type() !== LineType.DIAGONAL);
+let vents = lines.slice(0, -1).map(line => {
+	const lineArr = line.split(" -> ").map(point => {
+		const pointArr = point.split(",").map(Number);
+		return new Point(pointArr[0], pointArr[1]);
+	});
+	return new Line(lineArr[0], lineArr[1]);
+});
 
 for (const vent of vents) {
 	vent.span().forEach(point => {
@@ -77,5 +92,22 @@ for (const vent of vents) {
 	});
 }
 
-console.log(ventPoints.length);
-console.log(crossPoints.length);
+let part2 = crossPoints.length;
+
+vents = vents.filter(line => line.type() !== LineType.DIAGONAL);
+
+crossPoints = [];
+ventPoints = [];
+
+for (const vent of vents) {
+	vent.span().forEach(point => {
+		if (!ventPoints.some(v => v.equals(point))) {
+			ventPoints.push(point);
+		} else if (!crossPoints.some(v => v.equals(point))) {
+			crossPoints.push(point);
+		}
+	});
+}
+
+console.log("Part 1:", crossPoints.length);
+console.log("Part 2:", part2);
