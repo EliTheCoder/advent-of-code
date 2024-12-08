ff7d3a8733fe7dfc791a710d53eb51e1a06b0a16
Eli Frigo <eli@elifrigo.com>
2024-12-08 16:58:48 -0600

    Day 7

---
+++ 2024/day07/p1.py
@@ -0,0 +1,20 @@
+inp = open("input.txt", "r").read().splitlines()
+
+def parse(line):
+    result, factors = line.split(": ")
+    return int(result), [int(factor) for factor in factors.split()]
+
+rows = [parse(line) for line in inp]
+
+def calculate(result, factors):
+    if len(factors) <= 1: return result == factors[0]
+    if calculate(result, [factors[0] * factors[1]] + factors[2:]): return True
+    if calculate(result, [factors[0] + factors[1]] + factors[2:]): return True
+    return False
+
+total = 0
+
+for result, factors in rows:
+    if calculate(result, factors): total += result
+
+print(total)
